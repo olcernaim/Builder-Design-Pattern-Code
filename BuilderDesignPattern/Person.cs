7073408301ca2e3629f4a2b719adcb316fc731db
naimolcer <naimolcer@gmail.com>
2024-03-15 11:10:34 +0300

    Add project files.

---
+++ BuilderDesignPattern/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderDesignPattern
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public int phone { get; set; }
+        public string addresss { get; set; }
+        public string citno { get; set; }
+
+        public Person() { }
+
+        //public Person(string nameinput, string surnameinput, int ageinput)
+        //{
+        //    name = nameinput;
+        //    surname = surnameinput;
+        //    age = ageinput;
+        //}
+
+        //public Person(int phoneinput, string citnoinput, int ageinput)
+        //{
+        //    phone = phoneinput;
+        //    citno = citnoinput;
+        //    age = ageinput;
+        //}
+
+        //public Person(string nameinput, string surnameinput, int ageinput, int phoneinput, string citnoinput)
+        //{
+        //    name = nameinput;
+        //    surname = surnameinput;
+        //    age = ageinput;
+        //    phone = phoneinput;
+        //    citno = citnoinput;
+        //}
+
+        public void Print()
+        {
+            Console.WriteLine("name is : " + name);
+            Console.WriteLine("surname is : " + surname);
+            Console.WriteLine("age is : " + age);
+            Console.WriteLine("phone is : " + phone);
+            Console.WriteLine("addresss is : " + addresss);
+            Console.WriteLine("citno is : " + citno);
+        }
+
+    }
+
+    public class PersonBuilder
+    {
+        private Person _person = new Person();
+
+        public PersonBuilder SetName(string nameinput)
+        {
+            _person.name = nameinput;
+            return this;
+        }
+        public PersonBuilder SetSurname(string surnameinput)
+        {
+            _person.surname = surnameinput;
+            return this;
+        }
+        public PersonBuilder SetCitNo(string citnoinput)
+        {
+            _person.citno = citnoinput;
+            return this;
+        }
+        public PersonBuilder SetAge(int ageinput)
+        {
+            _person.age = ageinput;
+            return this;
+        }
+        public PersonBuilder SetPhone(int phoneinput)
+        {
+            _person.phone = phoneinput;
+            return this;
+        }
+        public PersonBuilder SetAddress(string adressinput)
+        {
+            _person.addresss = adressinput;
+            return this;
+        }
+
+        public Person Build()
+        {
+            return _person;
+        }
+    }
+}
---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person per1 = new Person("Naim", "Ölçer", 12);
+
+            //per1.Print();
+
+            //Person per2 = new Person(123, "12345", 12);
+
+            //per2.Print();
+
+            //Person per3 = new Person("Naim", "Ölçer", 12, 12345, "12345");
+
+            //per3.Print();
+
+            Person person1 = new PersonBuilder().SetName("Naim").SetSurname("Ölçer").SetAge(12).Build();
+
+            person1.Print();
+
+            Person person2 = new PersonBuilder().SetAge(12).SetPhone(123).SetCitNo("12312412").Build();
+
+            person2.Print();
+
+            Person person3 = new PersonBuilder().SetName("Naim").SetSurname("Ölçer").SetAge(12).SetPhone(12345).SetCitNo("12345").Build();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
